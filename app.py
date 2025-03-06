16db673977cc99cfd2a9ca3a471e2dff6a6f13ee
Kiranug <kiran.ug123@gmail.com>
2025-03-06 14:09:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def test_db():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
